2b97fdc32c771dc9b8c4e74c57ceb3dad4b0cc39
maromshriki <marom.shriki@gmail.com>
2026-08-02 13:28:59 +0300

    least privalege

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -167,7 +167,7 @@ resource "aws_iam_role_policy" "vpc_flow_logs" {
           "logs:DescribeLogGroups",
           "logs:DescribeLogStreams"
         ]
-        Resource =         "${aws_cloudwatch_log_group.vpc_flow_logs.arn}:*"
+        Resource ="${aws_cloudwatch_log_group.vpc_flow_logs.arn}:*"
 
       }
     ]
