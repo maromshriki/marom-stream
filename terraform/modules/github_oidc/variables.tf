23a326f6b0971fcafce39f2bf72a067082393d1d
maromshriki <marom.shriki@gmail.com>
2026-08-03 13:14:57 +0300

    fix asgoidc

--- terraform/modules/github_oidc/variables.tf
+++ terraform/modules/github_oidc/variables.tf
@@ -1,11 +1,11 @@
 variable "github_owner" {
   type    = string
-  default = "maromshriki"
+  
 }
 
 variable "github_repo" {
   type = string
-  default = "marom-stream"
+  
 }
 
 variable "role_name" {
