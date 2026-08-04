7b66ac3f17449303c6561e08e65a9341f7acbe64
maromshriki <marom.shriki@gmail.com>
2026-08-04 12:39:46 +0300

    m

--- terraform/modules/compute/asg.tf
+++ terraform/modules/compute/asg.tf
@@ -1,8 +1,8 @@
 resource "aws_autoscaling_group" "backend" {
   name                = "backend-asg"
-  min_size            = 1
-  max_size            = 2
-  desired_capacity    = 1
+  min_size            = 2
+  max_size            = 3
+  desired_capacity    = 2
   vpc_zone_identifier = var.private_subnet_ids
   target_group_arns   = [aws_lb_target_group.app_tg.arn]
   health_check_type   = "ELB"
@@ -21,9 +21,9 @@ resource "aws_autoscaling_group" "backend" {
 
 resource "aws_autoscaling_group" "frontend" {
   name                = "frontend-asg"
-  min_size            = 1
-  max_size            = 2
-  desired_capacity    = 1
+  min_size            = 2
+  max_size            = 3
+  desired_capacity    = 2
   vpc_zone_identifier = var.private_subnet_ids
   target_group_arns   = [aws_lb_target_group.frontend_tg.arn]
   health_check_type   = "ELB"
